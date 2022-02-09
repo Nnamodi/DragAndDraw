093409a4d86d9556b6fd9efe8f88d378d0d23915
Roland <nnamdiigede11@gmail.com>
2022-02-09 22:15:55 +0100

    Drawing on screen

---
+++ app/src/main/java/com/bignerdranch/android/draganddraw/Box.kt
@@ -0,0 +1,16 @@
+package com.bignerdranch.android.draganddraw
+
+import android.graphics.PointF
+
+class Box(private val start: PointF) {
+    var end: PointF = start
+
+    val left: Float
+        get() = start.x.coerceAtMost(end.x)
+    val right: Float
+        get() = start.x.coerceAtLeast(end.x)
+    val top: Float
+        get() = start.y.coerceAtMost(end.y)
+    val bottom: Float
+        get() = start.y.coerceAtLeast(end.y)
+}
