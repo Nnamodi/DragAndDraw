70adc28079516111acf5ade4f05903a03e0a6bfa
Roland <nnamdiigede11@gmail.com>
2022-02-25 22:32:04 +0100

    Rotating boxes with second finger

--- app/src/main/java/com/bignerdranch/android/draganddraw/BoxDrawingView.kt
+++ app/src/main/java/com/bignerdranch/android/draganddraw/BoxDrawingView.kt
@@ -1,5 +1,6 @@
 package com.bignerdranch.android.draganddraw
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
@@ -11,6 +12,8 @@ import android.util.Log
 import android.view.MotionEvent
 import android.view.View
 
+private const val INVALID_POINTER_ID = -1
+
 class BoxDrawingView(context: Context, attr: AttributeSet? = null) : View(context, attr) {
     private var currentBox: Box? = null
     private val boxen = mutableListOf<Box>()
@@ -20,38 +23,69 @@ class BoxDrawingView(context: Context, attr: AttributeSet? = null) : View(contex
     private val backgroundPaint = Paint().apply {
         color = 0xfff8efe0.toInt()
     }
-    private var boxId = this.apply {
-        id = R.id.view
-    }
-    private var boxes = boxId.id
+    private var boxes = this.id
+    private var posX = 0.0f ; private var posY = 0.0f
+    private var lastTouchX = 0.0f ; private var lastTouchY = 0.0f
+    // The 'active pointer' is the one currently moving our object.
+    private var activePointerId = INVALID_POINTER_ID
 
     init {
         isSaveEnabled = true
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onTouchEvent(event: MotionEvent): Boolean {
         val current = PointF(event.x, event.y)
         var action = ""
-        when (event.action) {
+        when (event.action and MotionEvent.ACTION_MASK) {
             MotionEvent.ACTION_DOWN -> {
                 action = "ACTION_DOWN"
                 // Reset drawing state
                 currentBox = Box(current).also {
                     boxen.add(it)
                 }
+                // Remember where we started
+                lastTouchX = event.x ; lastTouchY = event.y
+                // Save the ID of this pointer
+                activePointerId = event.getPointerId(0)
             }
             MotionEvent.ACTION_MOVE -> {
                 action = "ACTION_MOVE"
+                // Find the index of the active pointer and fetch its position
+                val pointerIndex = event.findPointerIndex(activePointerId)
+                val x = event.getX(pointerIndex)
+                val y = event.getY(pointerIndex)
+                // Calculate the distance moved
+                val dx = x - lastTouchX
+                val dy = y - lastTouchY
+                // Move the object
+                posX += dx ; posY += dy
+                // Remember this touch position for the next move event
+                lastTouchX = x ; lastTouchY = y
                 updateCurrentBox(current)
             }
             MotionEvent.ACTION_UP -> {
                 action = "ACTION_UP"
                 updateCurrentBox(current)
                 currentBox = null
+                activePointerId = INVALID_POINTER_ID
             }
             MotionEvent.ACTION_CANCEL -> {
                 action = "ACTION_CANCEL"
                 currentBox = null
+                activePointerId = INVALID_POINTER_ID
+            }
+            MotionEvent.ACTION_POINTER_UP -> {
+                // Extract the index of the pointer that left the touch sensor
+                val pointerIndex = (event.action and MotionEvent.ACTION_POINTER_INDEX_MASK) shr MotionEvent.ACTION_POINTER_INDEX_SHIFT
+                val pointerId = event.getPointerId(pointerIndex)
+                if (pointerId == activePointerId) {
+                    // This was our active pointer going up. Choose a new active pointer and adjust accordingly
+                    val newPointerIndex = if (pointerIndex == 0) 1 else 0
+                    lastTouchX = event.getX(newPointerIndex)
+                    lastTouchY = event.getY(newPointerIndex)
+                    activePointerId = event.getPointerId(newPointerIndex)
+                }
             }
         }
         Log.i("BoxDrawView", "$action at x = ${current.x}, y = ${current.y}")
@@ -61,6 +95,9 @@ class BoxDrawingView(context: Context, attr: AttributeSet? = null) : View(contex
     override fun onDraw(canvas: Canvas) {
         // Fill the background
         canvas.drawPaint(backgroundPaint)
+        canvas.save()
+        canvas.translate(posX, posY)
+        canvas.restore()
 
         boxen.forEach { box ->
             canvas.drawRect(box.left, box.top, box.right, box.bottom, boxPaint)
@@ -72,6 +109,7 @@ class BoxDrawingView(context: Context, attr: AttributeSet? = null) : View(contex
         val bundle = Bundle()
         bundle.putInt("points", boxes)
         bundle.putParcelable("superState", superState)
+        Log.d("SavedState", "$bundle")
         return bundle
     }
 
@@ -81,6 +119,7 @@ class BoxDrawingView(context: Context, attr: AttributeSet? = null) : View(contex
             boxes = view.getInt("points")
             view = view.getParcelable("superState")
         }
+        Log.d("SavedState", "$view, $boxes")
         super.onRestoreInstanceState(view)
     }
 
